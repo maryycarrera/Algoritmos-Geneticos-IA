8b13babaa6ef3b4943ae24749db07134623cf8a2
María Carrera <marcartal1@alum.us.es>
2024-06-06 19:27:39 +0200

    cruzar, mutar, seleccionar padres, mejor solución

--- src/ag/AG_marcartal1_natolmvil.py
+++ src/ag/AG_marcartal1_natolmvil.py
@@ -14,14 +14,14 @@ def AG(datos_train, datos_test, seed, nInd, maxIter):
     X_test = datos_test.iloc[:, :-1].values
 
     #Generar población inicial
-    nAtr = X_train.shape[1]
-    poblacion_inicial = poblacion_inicial(nAtr, nInd, seed)
+    num_atributos = X_train.shape[1]
+    poblacion_inicial = poblacion_inicial(num_atributos, nInd, seed)
 
     # Iterar
     pc = 0.7
     pm = 0.1
-    poblacion_solucion = algoritmo_genetico(poblacion_inicial, nInd, maxIter, pc, pm, nAtr, X_train, y_train)
-    mejor_individuo, y_pred = mejor(poblacion_solucion)
+    poblacion_solucion = algoritmo_genetico(poblacion_inicial, nInd, maxIter, pc, pm, X_train, y_train)
+    mejor_individuo, y_pred = mejor(poblacion_solucion, X_train, y_train, num_atributos)
 
     return mejor_individuo, y_pred
 
@@ -30,28 +30,29 @@ def poblacion_inicial(nAtributos, nInd, semilla):
     poblacion = np.random.randint(2, size=(nInd, nAtributos*2+1))
     return poblacion
 
-def algoritmo_genetico(pob, nInd, maxIter, pc, pm, nAtr, atr_train, obj_train):
-    evalu_pob = fitness_poblacion(pob, atr_train, obj_train, nAtr) 
+def algoritmo_genetico(pob, nInd, maxIter, pc, pm, atr_train, obj_train):
+    evalu_pob = fitness_poblacion(pob, atr_train, obj_train) 
     i=0
     while(i<maxIter):
-        padres = seleccionarPadres(evalu_pob,pob)
+        padres = seleccionarPadres(evalu_pob, pob)
         hijos = cruzar(padres, pc)
         hijos = mutar(hijos, pm)
-        evalu_hijos = fitness_poblacion(hijos, atr_train, obj_train, nAtr) 
+        evalu_hijos = fitness_poblacion(hijos, atr_train, obj_train) 
         pob = seleccionarSiguientePob(pob, hijos, evalu_pob, evalu_hijos)
         i = i+1
 
     return pob 
 
-def fitness_poblacion(poblacion, atr_train, obj_train, nAtr):
+def fitness_poblacion(poblacion, atr_train, obj_train):
     fitness = np.zeros(poblacion.shape[0])
 
     for i in range(poblacion.shape[0]):
-        fitness[i] = fitness(poblacion[i], atr_train, obj_train, nAtr)
+        fitness[i] = fitness(poblacion[i], atr_train, obj_train)
 
     return fitness
 
-def fitness(individuo, atr_train, obj_train, nAtr):
+def fitness(individuo, atr_train, obj_train):
+    nAtr = atr_train.shape[1]
     casos = atr_train.shape[0]
     y_pred = np.zeros(casos)
 
@@ -84,30 +85,22 @@ def regresion(coeficientes, nATr, caso):
 
     return sol
 
-def seleccionarPadres(evalu_pob,pob):
-    porcentaje=0.2
-    evalu_pob_ordenado = sorted(evalu_pob, reverse=True)
-    valores_maximos = evalu_pob_ordenado[:porcentaje_mejores_ind]
+def seleccionarPadres(evaluacion, pob):
+    k = 4
+    num_individuos = random.randint(k, pob.shape[0]-1)
+    padres = np.zeros((num_individuos, pob.shape[1]))
+    for i in range(num_individuos):
+        individuos = np.zeros((k, pob.shape[1]))
+        fitness = np.zeros(k)
+        for j in range(k):
+            rand = random.randint(0, num_individuos-1)
+            individuos[j] = pob[rand]
+            fitness[j] = evaluacion[rand]
+        
+        index_mejor_individuo = np.argmax(fitness)
+        padres[i] = individuos[index_mejor_individuo]
 
-    # Emparejar fitness con individuos y ordenarlos por fitness en orden descendente
-    evalu_pob_con_indices = list(enumerate(evalu_pob))
-    evalu_pob_con_indices.sort(key=lambda x: x[1], reverse=True)
-    
-    # Calcular el número de individuos a mantener como elitismo
-    total_ind = len(evalu_pob)
-    porcentaje_mejores_ind = int(total_ind * 0.2)
-    
-    # Obtener los índices de los mejores individuos
-    indices_elitismo = [idx for idx, _ in evalu_pob_con_indices[:cantidad_elitismo]]
-    
-    # Obtener los individuos que se mantendrán como elitismo
-    mejores_individuos = [pob[idx] for idx in indices_elitismo]
-    
-    # Obtener los individuos que serán cruzados (excluyendo los de elitismo)
-    indices_cruce = [idx for idx, _ in evalu_pob_con_indices[cantidad_elitismo:]]
-    individuos_cruce = [pob[idx] for idx in indices_cruce]
-    
-    return mejores_individuos, individuos_cruce
+    return padres
 
 def cruzar(padres, pc):
     punto_cruce = padres.shape[1] / 2
@@ -132,3 +125,56 @@ def cruzar(padres, pc):
         i = i+2
     
     return hijos
+
+def mutar(pob, pm):
+    i = 0
+
+    while(i<pob.shape[0]):
+        individuo = pob[i]
+        rand = random.random()
+
+        if(rand < pm):
+            gen = random.randint(0, individuo.shape[0]-1)
+            individuo[gen] = 1 - individuo[gen]
+
+        i = i+1
+
+    return pob
+
+def seleccionarSiguientePob(pob, hijos, evalu_pob, evalu_hijos):
+    porcentaje=0.2
+    evalu_pob_ordenado = sorted(evalu_pob, reverse=True)
+    valores_maximos = evalu_pob_ordenado[:porcentaje_mejores_ind]
+
+    # Emparejar fitness con individuos y ordenarlos por fitness en orden descendente
+    evalu_pob_con_indices = list(enumerate(evalu_pob))
+    evalu_pob_con_indices.sort(key=lambda x: x[1], reverse=True)
+    
+    # Calcular el número de individuos a mantener como elitismo
+    total_ind = len(evalu_pob)
+    porcentaje_mejores_ind = int(total_ind * 0.2)
+    
+    # Obtener los índices de los mejores individuos
+    indices_elitismo = [idx for idx, _ in evalu_pob_con_indices[:cantidad_elitismo]]
+    
+    # Obtener los individuos que se mantendrán como elitismo
+    mejores_individuos = [pob[idx] for idx in indices_elitismo]
+    
+    # Obtener los individuos que serán cruzados (excluyendo los de elitismo)
+    indices_cruce = [idx for idx, _ in evalu_pob_con_indices[cantidad_elitismo:]]
+    individuos_cruce = [pob[idx] for idx in indices_cruce]
+    
+    return mejores_individuos, individuos_cruce
+
+def mejor(poblacion, atr_train, obj_train):
+    mejor = 0
+    mejor_individuo = np.zeros(poblacion.shape[1])
+    for i in range(poblacion.shape[0]):
+        fit = fitness(poblacion[i], atr_train, obj_train)
+        if(fit > mejor):
+            mejor = fit
+            mejor_individuo = poblacion[i]
+
+    y_pred = regresion(mejor_individuo, atr_train.shape[1], atr_train)
+
+    return mejor_individuo, y_pred
